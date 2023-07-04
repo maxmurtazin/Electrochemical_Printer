97cd8a8f0d08cf953917d571e27107a9410ddd72
Murtazin Maxim <murtazinmaxim@outlook.com>
2023-07-04 12:16:11 +0300

    2

--- Read_current/Read_current.ino
+++ Read_current/Read_current.ino
@@ -31,9 +31,9 @@ void setup()
  // Initialize the INA219.
   // By default the initialization will use the largest range (32V, 2A).  However
   // you can call a setCalibration function to change this range (see comments).
-  // if (! ina219.begin()) {
-  //   Serial.println("Failed to find INA219 chip");
-  //   while (1) { delay(10); }
+  //  if (! ina219.begin()) {
+  //    Serial.println("Failed to find INA219 chip");
+  //    while (1) { delay(10); }
   // }
   // To use a slightly lower 32V, 1A range (higher precision on amps):
   ina219.setCalibration_32V_1A();
